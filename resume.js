03c7aef6655e10a2f7b5bfd7cad9f0b4a59d0b88
Aryan <aryantiwari209@gmail.com>
2022-10-13 21:20:03 +0530

    Added vertical orientation support

--- resume.js
+++ resume.js
@@ -1,6 +1,20 @@
 function changeVisibility(window) {
+    const proj = document.getElementById('projects');
+    const res = document.getElementById('resume');
+    
     const elem = document.getElementById(window);
-    if (elem.style.display == "none"){
+    console.log(window)
+    console.log(elem.style.display)
+
+    
+    if (window == 'resume' && proj.style.display == "block"){
+        proj.style.display = "none";
+    }
+    else if (window == 'projects' && resume.style.display == "block"){
+        res.style.display = "none";
+    }
+
+    if (elem.style.display == "none" || elem.style.display == ""){
         elem.style.display = "block";
     }
     else{
